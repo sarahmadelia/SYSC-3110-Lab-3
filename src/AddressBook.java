4198a10be93c12239b809af4654e62e72a6cbc42
sarahmadelia <sarah.abdallah99@outlook.com>
2020-09-25 10:19:51 -0400

    I add some function calls to AddressBook.java to test Git Commit

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -17,9 +17,6 @@ public void removeBuddy(BuddyInfo buddy){
     this.removeElement(buddy);
 }
     public static void main(String[] args){
-    BuddyInfo buddy = new BuddyInfo("Tom","Carleton", "613");
-    AddressBook addressBook= new AddressBook();
-    addressBook.addBuddy(buddy);
-    addressBook.removeBuddy(buddy);
+    System.out.println("Address Book");
     }
 }
