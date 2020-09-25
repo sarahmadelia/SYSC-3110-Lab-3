c0be6ae76abd3c70e8902cd9147b1d91a8a72d6d
sarahmadelia <sarah.abdallah99@outlook.com>
2020-09-25 10:14:39 -0400

    I add some function calls to AddressBook.java to test Git Commit and change methods

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -16,18 +16,6 @@ public AddressBook(AddressBook address){
 public void removeBuddy(BuddyInfo buddy){
     this.removeElement(buddy);
 }
-    public void addBuddy(AddressBook address){
-    for(int i=0; i<address.size(); i++) {
-        this.addBuddy(address.get(i));
-    }
-    }
-
-    public void removeBuddy(int index){
-    if(index>=0) {
-        this.remove(index);
-    }
-    }
-
     public static void main(String[] args){
     BuddyInfo buddy = new BuddyInfo("Tom","Carleton", "613");
     AddressBook addressBook= new AddressBook();
